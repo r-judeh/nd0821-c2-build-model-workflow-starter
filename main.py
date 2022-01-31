c7892a04cf44801f700b110edc26f181b543990a
Raja Judeh <judeh.raja@outlook.com>
2022-01-31 22:54:08 +0200

    fix typo

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def go(config: DictConfig):
         if "test_regression_model" in active_steps:
 
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model"
+                f"{config['main']['components_repository']}/test_regression_model",
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
