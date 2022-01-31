1f681b7093a82f5931535b49ceec5f0cb306fa9f
Raja Judeh <judeh.raja@outlook.com>
2022-01-31 14:39:43 +0200

    add two additional tests to data_check

--- src/data_check/conftest.py
+++ src/data_check/conftest.py
@@ -15,8 +15,7 @@ def pytest_addoption(parser):
 def data(request):
     run = wandb.init(job_type="data_tests", resume=True)
 
-    # Download input artifact. This will also note that this script is using this
-    # particular version of the artifact
+    # Download input artifact.
     data_path = run.use_artifact(request.config.option.csv).file()
 
     if data_path is None:
@@ -52,6 +51,7 @@ def kl_threshold(request):
 
     return float(kl_threshold)
 
+
 @pytest.fixture(scope='session')
 def min_price(request):
     min_price = request.config.option.min_price
@@ -60,6 +60,7 @@ def min_price(request):
         pytest.fail("You must provide min_price")
 
     return float(min_price)
+
 
 @pytest.fixture(scope='session')
 def max_price(request):
