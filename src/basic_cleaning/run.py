4ff599aff079d5c94b60dc5b098e3b8fe03e13d1
Raja Judeh <judeh.raja@outlook.com>
2022-02-01 13:00:37 +0200

    remove converting the last_raw from basic_cleaning, add proper rgeolocation filtering

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,10 +33,9 @@ def go(args):
         idx = df['price'].between(args.min_price, args.max_price)
         df = df[idx].copy()
 
-        # Convert the last_review column to datetime
-        logger.info("Convert last_review column to datetime")
-
-        df['last_review'] = pd.to_datetime(df['last_review'])
+        # Drop data that are not in proper geolocation
+        idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+        df = df[idx].copy()
 
         # Save the results
         logger.info("Save the new dataframe to clean_sample.csv")
